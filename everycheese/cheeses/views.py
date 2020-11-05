a0e8aab78d47e07d18701009e69a473ef4e26442
vacyny <k@lomati.ru>
2020-11-05 14:22:12 +0300

    Реализовать страницу со списком сыров, добавить ссылку на навигационную панель

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Cheese
+
+class CheeseListView(ListView):
+    model = Cheese
