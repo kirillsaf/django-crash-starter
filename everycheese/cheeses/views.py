3d6fafc30541f17e9b11bc082482399880c3afe0
vacyny <k@lomati.ru>
2020-11-06 18:43:35 +0300

    Ограничить форму добавления сыра только зарегистрированным пользователям

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -3,6 +3,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Cheese
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CheeseListView(ListView):
     model = Cheese
@@ -10,6 +11,6 @@ class CheeseListView(ListView):
 class CheeseDetailView(DetailView):
     model = Cheese
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
     fields = ['name', 'description', 'firmness', 'country_of_origin']
